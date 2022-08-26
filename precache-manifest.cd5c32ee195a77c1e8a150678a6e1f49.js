d58034a6d0ee2c0338b7912b7ce393694935de84
Anna Cai <annacai44@gmail.com>
2022-08-26 14:38:46 -0500

    Updates

---
+++ precache-manifest.cd5c32ee195a77c1e8a150678a6e1f49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c273830070b74e29411ed1afcaab412",
+    "url": "/FoodFinder/index.html"
+  },
+  {
+    "revision": "0a7693ffd43b7ce10496",
+    "url": "/FoodFinder/static/css/main.0e60e1bc.chunk.css"
+  },
+  {
+    "revision": "50d5c49a4696f8e73345",
+    "url": "/FoodFinder/static/js/2.26bf0eaa.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/FoodFinder/static/js/2.26bf0eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7693ffd43b7ce10496",
+    "url": "/FoodFinder/static/js/main.662769a1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/FoodFinder/static/js/main.662769a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77b9d6b1a8ecc44e1c8",
+    "url": "/FoodFinder/static/js/runtime-main.a4e6916a.js"
+  }
+]);
